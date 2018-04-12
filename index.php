18bfeb8bd50bf3087df3aaa196ccd3c0bdf6675b
Leandro Carvalho <skateforever@users.noreply.github.com>
2018-04-12 17:22:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
   <title>Teste PHP</title>
  </head>
  <body>
- <?php echo "<p>Olá Mundo</p>"; ?>
+ <?php
+  echo "<p>Olá Mundo</p>"; 
+  echo "<h1>Openshift Workshop v1.0</h1> ";
+  echo $_SERVER['SERVER_ADDR'];
+  ?>
  </body>
 </html>
